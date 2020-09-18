9845c4ae82a7192b198033a9cd297faec299ab6e
Douglas Walas <douglas.skd@gmail.com>
2020-09-17 23:07:04 -0300

    testing app deploy

---
+++ xcbot/react/components/Informations/Informations.tsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import { Loading } from 'react-simple-chatbot';
+
+interface InformationsProps { }
+
+interface Orders {
+  orderId: string
+  statusDescription: string
+  value: number
+  erro: string
+}
+
+const Informations: StorefrontFunctionComponent<InformationsProps> = ({ }) => {
+  const [data, setData] = useState<Array<Orders>>([])
+  const orderId = localStorage.getItem('orderId')
+
+  useEffect(() => {
+    async function loading() {
+      const result = await axios.get(`https://xcoders-gama.herokuapp.com/orders/${orderId}`)
+      console.log(result.data)
+      const requisition: Array<Orders> = [result.data]
+      setData(requisition)
+    }
+    loading()
+  }, [orderId])
+
+  return (
+    <>
+      {
+        data !== [] ? data.map((item: Orders) => item.erro ? item.erro : <div>
+          <h3>Informações do Pedido</h3>
+          <p>Numero: {item.orderId}</p>
+          <p>Status: {item.statusDescription}</p>
+          <p>Valor: R$ {item.value}</p>
+        </div>
+        ) : <Loading />
+      }
+    </>
+  );
+}
+
+Informations.schema = {
+  title: 'ChatBotInfos',
+  description: 'Testando ChatBotInfos',
+  type: 'object',
+  properties: {},
+}
+
+export default Informations
