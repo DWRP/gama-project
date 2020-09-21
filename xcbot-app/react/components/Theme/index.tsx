2594df06ad45a11aa769c7bbddbdedbb939920dd
Douglas Walas <douglas.skd@gmail.com>
2020-09-20 23:10:57 -0300

    upgrade apps and fix erros

---
+++ xcbot-app/react/components/Theme/index.tsx
@@ -0,0 +1,32 @@
+const Theme = {
+    bubbleStyle:{
+        fontSize: '16px',
+        fontFamily: 'Arial',
+    },
+    floatingStyle:{
+        right: '8px',
+        bottom: '8px'
+    },
+    bubbleOptionStyle:{
+        background: '#5fb55e',
+        cursor: 'pointer'
+    },
+    theme:{
+        background: '#f5f8fb',
+        fontFamily: 'Helvetica Neue',
+        headerBgColor: '#0b305a',
+        headerFontColor: '#fff',
+        headerFontSize: '20px',
+        botBubbleColor: '#0b305a',
+        botFontColor: '#fff',
+        userBubbleColor: '#0b7e5b',
+        userFontColor: '#fff'
+    },
+    infos:{
+        background:"#f5f8fb",
+        width:"100%",
+        height:"100%"
+    }
+}
+
+export default Theme
