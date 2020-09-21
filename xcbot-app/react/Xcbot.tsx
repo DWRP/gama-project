152cbf2667924d9279554f28a1514ffa291c19e2
Douglas Walas Rodrigues Pardim <douglas.skd@gmail.com>
2020-09-20 23:25:24 -0300

    Revert "Feature/remake update"

--- xcbot-app/react/Xcbot.tsx
+++ xcbot-app/react/Xcbot.tsx
@@ -1,33 +1,130 @@
 import React from 'react'
-import ChatBot from 'react-simple-chatbot'
-import { ThemeProvider } from 'styled-components'
-import Theme from './components/Theme'
+import ChatBot from 'react-simple-chatbot';
+import { ThemeProvider } from 'styled-components';
 import Informations from './components/Informations/Informations'
-import stepsBase from './components/Steps'
 
-interface XcbotProps {
-  chatName: string
-  avatarIcon: string
-  chatIcon: string
+interface XcbotProps { }
+
+const bubbleStyle = {
+  fontSize: '16px',
+  fontFamily: 'Arial',
 }
+const floatingStyle = {
+  right: '8px',
+  bottom: '8px'
+}
+const bubbleOptionStyle = {
+  background: '#5fb55e',
+  cursor: 'pointer'
+}
+const theme = {
+  background: '#f5f8fb',
+  fontFamily: 'Helvetica Neue',
+  headerBgColor: '#57DA56',
+  headerFontColor: '#fff',
+  headerFontSize: '20px',
+  botBubbleColor: '#57DA56',
+  botFontColor: '#fff',
+  userBubbleColor: '#fff',
+  userFontColor: '#4a4a4a',
+};
 
-const Xcbot: StorefrontFunctionComponent<XcbotProps> = ({ chatName, avatarIcon, chatIcon }) => {
 
-  const steps = stepsBase(Informations)
+const Xcbot: StorefrontFunctionComponent<XcbotProps> = ({ }) => {
+
+  const steps = [
+    {
+      id: '1',
+      message: 'Olá, eu sou o Joseph, qual o seu nome?',
+      trigger: '2',
+    },
+    {
+      id: '2',
+      user: true,
+      trigger: '3',
+    },
+    {
+      id: '3',
+      message: 'Olá {previousValue}! Muito prazer',
+      trigger: '4',
+    },
+    {
+      id: '4',
+      message: 'Eu acabei de nascer e ainda estou aprendendo para ajudar muito mais, por enquanto, posso te ajudar com os seguintes assuntos abaixo',
+      trigger: 'type_select',
+    },
+    {
+      id: 'type_select',
+      options: [
+        {
+          value: 'rastreio',
+          label: 'Informações sobre Pedido',
+          trigger: '5'
+        },
+      ],
+    },
+    {
+      id: '5',
+      message: 'Certo! Vou te ajudar a encontrar seu pedido',
+      trigger: '6',
+    },
+
+
+    {
+      id: '6',
+      message: 'Por favor, informe o Numero do seu pedido',
+      trigger: '7',
+    },
+    {
+      id: '7',
+      user: true,
+      validator: (value: string) => {
+        localStorage.setItem('orderId', value)
+        return true
+      },
+      trigger: '8',
+    },
+    {
+      id: '8',
+      component: (<Informations />),
+      trigger: '9',
+    },
+    {
+      id: '9',
+      message: 'Espero que tenha conseguido te ajudar, Posso te ajudar com algo mais?',
+      trigger: '10',
+    },
+    {
+      id: '10',
+      options: [
+        {
+          value: 'sim',
+          label: 'Sim',
+          trigger: '11'
+        },
+        {
+          value: 'nao',
+          label: 'Não',
+          trigger: '12'
+        },
+      ],
+    },
+    {
+      id: '11',
+      message: 'Posso te ajudar com os seguintes assuntos abaixo',
+      trigger: 'type_select',
+    },
+    {
+      id: '12',
+      message: 'Obrigado pelo seu contato e até breve!',
+      end: true
+    }
+
+  ]
   return (
     <>
-      <ThemeProvider theme={Theme.theme}>
-        <ChatBot 
-          steps={steps}
-          botDelay="1000" 
-          bubbleOptionStyle={Theme.bubbleOptionStyle} 
-          floatingStyle={Theme.floatingStyle} 
-          bubbleStyle={Theme.bubbleStyle} 
-          headerTitle={chatName}
-          floatingIcon={avatarIcon}
-          botAvatar={chatIcon}
-          placeholder="Digite sua mensagem"
-          floating="true" />
+      <ThemeProvider theme={theme}>
+        <ChatBot steps={steps} botDelay="1800" bubbleOptionStyle={bubbleOptionStyle} floatingStyle={floatingStyle} bubbleStyle={bubbleStyle} headerTitle="ChatBot - XCoders" placeholder="Digite sua mensagem" floating="true" />
       </ThemeProvider>
     </>
   );
@@ -37,30 +134,7 @@ Xcbot.schema = {
   title: 'ChatBot',
   description: 'Testando ChatBoot',
   type: 'object',
-  properties: {
-    chatName:{
-      title: 'Titulo',
-      description: 'nome do chatbot',
-      type: 'string',
-      default:"ChatBot - XCoders"
-    },
-    avatarIcon:{
-      title: 'Avatar',
-      description: 'Avatar do bot',
-      type: 'string',
-      widget: {
-        'ui:widget': 'image-uploader',
-      },
-    },
-    chatIcon:{
-      title: 'Icone',
-      description: 'Icone do chat',
-      type: 'string',
-      widget: {
-        'ui:widget': 'image-uploader',
-      },
-    }
-  },
+  properties: {},
 }
 
 export default Xcbot
--- xcbot-app/react/components/Informations/Informations.tsx
+++ xcbot-app/react/components/Informations/Informations.tsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 import { Loading } from 'react-simple-chatbot';
 
+interface InformationsProps { }
+
 interface Orders {
   orderId: string
   statusDescription: string
@@ -9,45 +11,40 @@ interface Orders {
   erro: string
 }
 
-const Informations = () => {
-
-  const [loading, setLoading] = useState(true)
+const Informations: StorefrontFunctionComponent<InformationsProps> = ({ }) => {
   const [data, setData] = useState<Array<Orders>>([])
   const orderId = localStorage.getItem('orderId')
 
   useEffect(() => {
-    async function loader() {
+    async function loading() {
       const result = await axios.get(`https://xcoders-gama.herokuapp.com/orders/${orderId}`)
+      console.log(result.data)
       const requisition: Array<Orders> = [result.data]
       setData(requisition)
-      setLoading(false)
     }
-    loader()
+    loading()
   }, [orderId])
 
-  if(loading){
+  return (
+    <>
+      {
+        data !== [] ? data.map((item: Orders) => item.erro ? item.erro : <div>
+          <h3>Informações do Pedido</h3>
+          <p>Numero: {item.orderId}</p>
+          <p>Status: {item.statusDescription}</p>
+          <p>Valor: R$ {item.value}</p>
+        </div>
+        ) : <Loading />
+      }
+    </>
+  );
+}
 
-    return (
-      <div style={{display:"flex !important",flexDirection:"column",alignItems:"center",justifyContent:"center"}}>
-          <h4>Carregando informação</h4>
-          <Loading />
-      </div>
-    )
-  }else{
-    return (
-      <>
-        {
-          data.map((item: Orders) => item.erro ? item.erro : <div key={item.orderId}>
-              <h3>Informações do Pedido</h3>
-              <p>Numero: {item.orderId}</p>
-              <p>Status: {item.statusDescription}</p>
-              <p>Valor: R$ {item.value}</p>
-            </div>
-          )
-        }
-      </>
-    );
-  }
+Informations.schema = {
+  title: 'ChatBotInfos',
+  description: 'Testando ChatBotInfos',
+  type: 'object',
+  properties: {},
 }
 
 export default Informations
