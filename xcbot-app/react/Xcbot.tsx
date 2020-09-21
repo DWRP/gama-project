2267e421e033d268194d9ad25d9bd97995b2a9aa
Jorge Henrique <jorgehenrique@live.com>
2020-09-21 16:34:45 -0300

    Implementa Amplify AWS

--- xcbot-app/react/Xcbot.tsx
+++ xcbot-app/react/Xcbot.tsx
@@ -1,140 +1,48 @@
-import React from 'react'
-import ChatBot from 'react-simple-chatbot';
-import { ThemeProvider } from 'styled-components';
-import Informations from './components/Informations/Informations'
+import React, { Component } from 'react';
+import Amplify from 'aws-amplify';
+import { ChatBot, AmplifyTheme } from './node_modules/aws-amplify-react';
+import awsconfig from './aws-exports';
 
-interface XcbotProps { }
+Amplify.configure(awsconfig);
 
-const bubbleStyle = {
-  fontSize: '16px',
-  fontFamily: 'Arial',
-}
-const floatingStyle = {
-  right: '8px',
-  bottom: '8px'
-}
-const bubbleOptionStyle = {
-  background: '#5fb55e',
-  cursor: 'pointer'
-}
-const theme = {
-  background: '#f5f8fb',
-  fontFamily: 'Helvetica Neue',
-  headerBgColor: '#57DA56',
-  headerFontColor: '#fff',
-  headerFontSize: '20px',
-  botBubbleColor: '#57DA56',
-  botFontColor: '#fff',
-  userBubbleColor: '#fff',
-  userFontColor: '#4a4a4a',
+// Imported default theme can be customized by overloading attributes
+const myTheme = {
+  ...AmplifyTheme,
+  sectionHeader: {
+    ...AmplifyTheme.sectionHeader,
+    backgroundColor: '#ff6600'
+  }
 };
 
-
-const Xcbot: StorefrontFunctionComponent<XcbotProps> = ({ }) => {
-
-  const steps = [
-    {
-      id: '1',
-      message: 'Olá, eu sou o Joseph, qual o seu nome?',
-      trigger: '2',
-    },
-    {
-      id: '2',
-      user: true,
-      trigger: '3',
-    },
-    {
-      id: '3',
-      message: 'Olá {previousValue}! Muito prazer',
-      trigger: '4',
-    },
-    {
-      id: '4',
-      message: 'Eu acabei de nascer e ainda estou aprendendo para ajudar muito mais, por enquanto, posso te ajudar com os seguintes assuntos abaixo',
-      trigger: 'type_select',
-    },
-    {
-      id: 'type_select',
-      options: [
-        {
-          value: 'rastreio',
-          label: 'Informações sobre Pedido',
-          trigger: '5'
-        },
-      ],
-    },
-    {
-      id: '5',
-      message: 'Certo! Vou te ajudar a encontrar seu pedido',
-      trigger: '6',
-    },
-
-
-    {
-      id: '6',
-      message: 'Por favor, informe o Numero do seu pedido',
-      trigger: '7',
-    },
-    {
-      id: '7',
-      user: true,
-      validator: (value: string) => {
-        localStorage.setItem('orderId', value)
-        return true
-      },
-      trigger: '8',
-    },
-    {
-      id: '8',
-      component: (<Informations />),
-      trigger: '9',
-    },
-    {
-      id: '9',
-      message: 'Espero que tenha conseguido te ajudar, Posso te ajudar com algo mais?',
-      trigger: '10',
-    },
-    {
-      id: '10',
-      options: [
-        {
-          value: 'sim',
-          label: 'Sim',
-          trigger: '11'
-        },
-        {
-          value: 'nao',
-          label: 'Não',
-          trigger: '12'
-        },
-      ],
-    },
-    {
-      id: '11',
-      message: 'Posso te ajudar com os seguintes assuntos abaixo',
-      trigger: 'type_select',
-    },
-    {
-      id: '12',
-      message: 'Obrigado pelo seu contato e até breve!',
-      end: true
+class App extends Component {
+  handleComplete(err: any, confirmation: object) {
+    if (err) {
+      alert('Bot conversation failed')
+      return;
     }
 
-  ]
-  return (
-    <>
-      <ThemeProvider theme={theme}>
-        <ChatBot steps={steps} botDelay="1800" bubbleOptionStyle={bubbleOptionStyle} floatingStyle={floatingStyle} bubbleStyle={bubbleStyle} headerTitle="ChatBot - XCoders" placeholder="Digite sua mensagem" floating="true" />
-      </ThemeProvider>
-    </>
-  );
+    alert('Success: ' + JSON.stringify(confirmation, null, 2));
+    return 'Trip booked. Thank you! what would you like to do next?';
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to ChatBot Demo</h1>
+        </header>
+        <ChatBot
+          title="My Bot"
+          theme={myTheme}
+          botName="OrderFlowers_dev"
+          welcomeMessage="Welcome, how can I help you today?"
+          onComplete={this.handleComplete.bind(this)}
+          clearOnComplete={true}
+          conversationModeOn={false}
+        />
+      </div>
+    );
+  }
 }
 
-Xcbot.schema = {
-  title: 'ChatBot',
-  description: 'Testando ChatBoot',
-  type: 'object',
-  properties: {},
-}
-
-export default Xcbot
+export default App;
