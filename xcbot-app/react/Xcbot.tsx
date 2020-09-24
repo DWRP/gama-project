12231083eccd5c10aa69890ce276d7771ecf558e
Douglas Walas <douglas.skd@gmail.com>
2020-09-24 11:55:19 -0300

    finish bot configs

--- xcbot-app/react/Xcbot.tsx
+++ xcbot-app/react/Xcbot.tsx
@@ -1,64 +1,149 @@
 import React, { useState, useEffect } from 'react';
 import Amplify, { Interactions } from 'aws-amplify';
 import awsconfig from './aws-exports';
-import { Widget, addResponseMessage, renderCustomComponent } from 'react-chat-widget';
+import { Widget, addResponseMessage, addUserMessage, renderCustomComponent } from 'react-chat-widget';
 import './utils/style.global.css';
 
 Amplify.configure(awsconfig);
 
-interface ChatProps { }
+interface ChatProps { 
+  chatName: string
+  avatarIcon: string
+  placeHolder: string
+}
 interface CardProps {
-  img?: string
-  title?: string
+  id: number
+  title: string
+  subTitle: string
+  imageUrl: string
+  price: number | string
+  attachmentLinkUrl: string
+}
+
+interface OrderProps{
+  orderId: string
+  statusDescription: string
+  erro: string
 }
 
 const Card = (props: any) => {
   let [data, setData] = useState<CardProps[]>([])
 
   useEffect(() => {
-    let newData: CardProps[] = []
-    Object.keys(props).map((key: any) => {
-      newData.push({ img: props[key].imageUrl, title: props[key].title })
-    })
-
+    let newData:CardProps[] = JSON.parse(props.message)
     setData(newData)
   }, [])
 
   return (
     <div>
       {
-        data.map((item: CardProps, index: number) => <div key={index}><h3>{item.title}</h3><img src={item.img} alt="" style={{ maxWidth: "40px", maxHeight: "40px", }} /></div>)
+        data.map((item: CardProps) => {
+          
+          return (
+            <div key={item.id}>
+              <h4>{item.title}</h4>
+              <img src={item.imageUrl} alt="" />
+              <h5>R$ {item.price}</h5>
+            </div>
+          )
+        })
       }
     </div>
   )
 }
 
-const Chat: StorefrontFunctionComponent<ChatProps> = ({ }) => {
+const Order = (props: any) => {
+  let [data, setData] = useState<OrderProps[]>([])
+
+  useEffect(() => {
+    let newData:OrderProps[] = JSON.parse(props.message)
+    setData(newData)
+    console.log(newData)
+  }, [])
+
+  return (
+    <div>
+      {
+        data.map((item: OrderProps, index: number) => {
+          if(item.erro){
+            return (
+              <h3>{item.erro}</h3>
+            )
+          }
+          else{
+            return (
+              <div key={index}>
+                <h4>{item.orderId}</h4>
+                <h5>{item.statusDescription}</h5>
+              </div>
+            )
+          }
+        })
+      }
+    </div>
+  )
+}
+
+
+const Chat: StorefrontFunctionComponent<ChatProps> = ({ chatName, avatarIcon, placeHolder }) => {
 
   async function handleMsg(input: any) {
 
     const response = await Interactions.send(awsconfig.aws_bots_config[0].name, input);
+    
     console.log(response)
-    if (response.responseCard) {
-      addResponseMessage(response.message)
-      renderCustomComponent(Card, response.responseCard.genericAttachments)
-      addResponseMessage("Essas são as opções")
+    let product = undefined
+    let numPedido =  undefined
+
+    if(response.slots !== undefined){
+      product = response.slots.product
+      numPedido =  response.slots.numPedido
     }
-    else {
-      addResponseMessage(response.message)
+    
+    if(product){
+      renderCustomComponent(Card, response)
+      return
     }
+    
+    if(numPedido){
+      renderCustomComponent(Order, response)
+      return
+    }
+    
+    addResponseMessage(response.message)
+  
   }
 
   useEffect(() => {
     addResponseMessage('Olá, em que posso te ajudar?')
+    renderCustomComponent(()=>{
+      function handleOption(option:string) {
+          addUserMessage(option)
+          handleMsg(option)
+      }
+      return (
+        <div>
+          <button onClick={()=>{
+            handleOption('comprar flores')
+          }}>flores</button>
+          <button onClick={()=>{
+            handleOption('Comprar produtos')
+          }}>produtos</button>
+          <button onClick={()=>{
+            handleOption('Comprar')
+          }}>categoria</button>
+        </div>
+      )
+    },'')
   }, [])
+
   return (
     <Widget
       handleNewUserMessage={(event: any) => handleMsg(event)}
-      title="Chatbot"
-      senderPlaceHolder="Escreva sua mensagem"
+      title={chatName}
+      senderPlaceHolder={placeHolder}
       subtitle={false}
-      profileAvatar="https://hiringcoders9.vtexassets.com/assets/vtex.file-manager-graphql/images/133ad9db-b1d4-4fa8-878a-00635dbaaa60___2ed90089707038a3e798b66150cdd1c5.png"
+      profileAvatar={avatarIcon}
       showCloseButton={true}
 
     />
@@ -66,4 +151,32 @@ const Chat: StorefrontFunctionComponent<ChatProps> = ({ }) => {
 
 }
 
+Chat.schema = {
+  title: 'ChatBot',
+  description: 'Testando ChatBoot',
+  type: 'object',
+  properties: {
+    chatName:{
+      title: 'Titulo',
+      description: 'nome do chatbot',
+      type: 'string',
+      default:"Chatbot"
+    },
+    avatarIcon:{
+      title: 'Avatar',
+      description: 'Avatar do bot',
+      type: 'string',
+      default:"https://hiringcoders9.vtexassets.com/assets/vtex.file-manager-graphql/images/133ad9db-b1d4-4fa8-878a-00635dbaaa60___2ed90089707038a3e798b66150cdd1c5.png",
+      widget: {
+        'ui:widget': 'image-uploader',
+      },
+    },
+    placeHolder:{
+      title: 'Place Holder',
+      description: 'Observação da caixa de texto do chat',
+      type: 'string',
+      default:"Escreva sua mensagem"
+    },
+  },
+}
 export default Chat;
