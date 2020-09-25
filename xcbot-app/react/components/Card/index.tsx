c549b0a721d612f2f9343125981e188d5172e2e8
Jorge Henrique <jorgehenrique@live.com>
2020-09-25 07:07:52 -0300

    Card Custom

--- xcbot-app/react/components/Card/index.tsx
+++ xcbot-app/react/components/Card/index.tsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { CardProps } from '../../utils/interfaces';
+import { FaPlus, FaMinus, FaCartPlus } from 'react-icons/fa';
 
 export const Card = (props: any) => {
-  let [data, setData] = useState<CardProps[]>([])
+  let [data, setData] = useState<CardProps[]>([]);
+  let finishOrder = false;
 
   useEffect(() => {
     let newData: CardProps[] = JSON.parse(props.message)
@@ -10,20 +12,39 @@ export const Card = (props: any) => {
   }, [])
 
   return (
-    <div className="box-container">
-      {
-        (data ?
-          data.map((item: CardProps) => {
-            return (
-              <div className="box-container-card" key={item.id}>
-                <h4>{item.title}</h4>
-                <img src={item.imageUrl} alt="" />
-                <h5>R$ {item.price}</h5>
-              </div>
+    <div className="order-box">
+      <div className="wrapper">
+        <div className="wrapper_child">
+          {
+            (data ?
+              data.map((item: CardProps) => {
+                { finishOrder = true }
+                return (
+                  <div className="tab" key={item.id}>
+                    <img src={item.imageUrl} width="100%" />
+                    <p className="title-box">{item.title}</p>
+                    <p className="price-box"><span>R$</span> {item.price}</p>
+                    <div className="buttons-qtd-box">
+                      <button className="minus"><FaMinus /></button>
+                      <input type="text" name="qtd" value="0" />
+                      <button className="plus"><FaPlus /></button>
+                    </div>
+                  </div>
+                )
+              })
+              :
+              <div>Produto não encontrado</div>
             )
-          })
+          }
+        </div>
+      </div>
+      {
+        (finishOrder ?
+          <div className="finish-order">
+            <button>Finalizar Compra <FaCartPlus /></button>
+          </div>
           :
-          <div>Produto não encontrado</div>
+          ''
         )
       }
     </div>
