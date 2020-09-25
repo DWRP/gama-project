7e226532610d017b29e852e2668d197a0dfa66dd
Douglas Walas <douglas.skd@gmail.com>
2020-09-25 13:52:04 -0300

    prerelease finish

--- xcbot-app/react/components/Card/index.tsx
+++ xcbot-app/react/components/Card/index.tsx
@@ -1,5 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { CardProps } from '../../utils/interfaces';
+import {
+  addResponseMessage
+}
+  from 'react-chat-widget';
+
 import { FaCartPlus } from 'react-icons/fa';
 import {
   ButtonSum
@@ -7,10 +12,11 @@ import {
   from './Button';
 export const Card = (props: any) => {
   let [data, setData] = useState<CardProps[]>([]);
-  let finishOrder = false;
+  let [finishOrder,setFinish] = useState(false);
 
   useEffect(() => {
     let newData: CardProps[] = JSON.parse(props.message)
+    localStorage.setItem('cartClosed','false')
     setData(newData)
   }, [])
 
@@ -19,28 +25,30 @@ export const Card = (props: any) => {
       <div className="wrapper">
         <div className="wrapper_child">
           {
-            (data ?
+            (data && !finishOrder?
               data.map((item: CardProps) => {
-                { finishOrder = true }
                 return (
                   <div className="tab" key={item.id}>
                     <img src={item.imageUrl} width="100%" />
                     <p className="title-box">{item.title}</p>
                     <p className="price-box"><span>R$</span> {item.price}</p>
-                    <ButtonSum key={item.id} />
+                    <ButtonSum id={item.id} />
                   </div>
                 )
               })
-              :
-              <div>Produto não encontrado</div>
+              :finishOrder? <div>Carrinho fechado!</div> :<div>Produto não encontrado</div>
             )
           }
         </div>
       </div>
       {
-        (finishOrder ?
+        (!finishOrder ?
           <div className="finish-order">
-            <button>Finalizar Compra <FaCartPlus /></button>
+            <button onClick={()=>{
+              localStorage.setItem('cartClosed','true')
+              setFinish(true)
+              addResponseMessage('Vamos continuar a compra.\nPor favor, informe seu email!')
+              }}>Fechar carrinho<FaCartPlus /></button>
           </div>
           :
           ''
